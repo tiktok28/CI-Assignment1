d33423f854fc8f6f698c8031a25cb276bac1f1d3
AusPin <103032413+AusPin@users.noreply.github.com>
2023-10-18 22:35:07 +0800

    Update EnergyConsumptionAI.py

--- EnergyConsumptionAI.py
+++ EnergyConsumptionAI.py
@@ -1,1 +1,10 @@
 print("I gae")
+
+import numpy as np
+from skfuzzy import control as ctrl
+from skfuzzy import membership as mf
+
+indoor_light_level = ctrl.Antecedent(np.arange(0, 10, 0.1), 'indoor_light_level')
+time_of_day = ctrl.Antecedent(np.arange(0, 10, 0.1), 'time_of_day')
+people_nearby = ctrl.Antecedent(np.arange(0, 10, 0.1), 'people_nearby')
+light_output = ctrl.Consequent(np.arange(0, 30, 0.1), 'light_output')
